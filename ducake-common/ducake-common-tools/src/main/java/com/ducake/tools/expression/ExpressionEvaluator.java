dbaf1ccd3bc0d32bef1899907a660e270e7387bc
dujunli <934774803@qq.com>
2023-07-05 20:22:34 +0800

    20230628 | 覆写determineCurrentLookupKey()实现运行时添加数据源

--- ducake-common/ducake-common-tools/src/main/java/com/ducake/tools/expression/ExpressionEvaluator.java
+++ ducake-common/ducake-common-tools/src/main/java/com/ducake/tools/expression/ExpressionEvaluator.java
@@ -45,19 +45,7 @@ public class ExpressionEvaluator<T> extends CachedExpressionEvaluator {
      */
     private Method getTargetMethod(Class<?> targetClass, Method method) {
         AnnotatedElementKey methodKey = new AnnotatedElementKey(method, targetClass);
-
-        /**
-         * 根据sonar建议可优化为：
-         * return this.targetMethodCache.computeIfAbsent(methodKey, k -> AopUtils.getMostSpecificMethod(method, targetClass));
-         * 但是我就不
-         */
-
-        Method targetMethod = this.targetMethodCache.get(methodKey);
-        if (targetMethod == null) {
-            targetMethod = AopUtils.getMostSpecificMethod(method, targetClass);
-            this.targetMethodCache.put(methodKey, targetMethod);
-        }
-        return targetMethod;
+        return this.targetMethodCache.computeIfAbsent(methodKey, k -> AopUtils.getMostSpecificMethod(method, targetClass));
     }
 
     /**
